fe16b1edf7b852c95db8064c2184e4a0f13e8b87
Fazalcs13 <Fazalcs13@gmail.com>
2017-11-14 21:15:06 +0500

    Chatterbot with flask

--- app.py
+++ app.py
@@ -4,10 +4,29 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 
 app = Flask(__name__)
 
-english_bot = ChatBot("English Bot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+"""english_bot = ChatBot("English Bot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
 
 english_bot.set_trainer(ChatterBotCorpusTrainer)
-english_bot.train("chatterbot.corpus.english")
+english_bot.train("chatterbot.corpus.english")"""
+
+english_bot = ChatBot(
+    'cds',
+    trainer='chatterbot.trainers.ListTrainer'
+)
+
+english_bot.train([
+
+    "I would to run a security test on web, need to identify best tool",
+    "You can use https://www.websecurify.com/ to test the website vlunaribility.",
+    "What is CDS",
+    "CDS is an software company.",
+    "I need help with Photoshop",
+    "Here is the link to Photoshop http://www.adobe.com/products/photoshop.html",
+    "I need help with Graphic Design",
+    "Here is the link to Graphic Design https://www.behance.net/search?field=44",
+    "I need help with Coding",
+    "Here is the link to Coding http://www.codeconquest.com/what-is-coding/"
+])
 
 
 @app.route("/")
